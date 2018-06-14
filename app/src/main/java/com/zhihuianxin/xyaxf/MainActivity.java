2913c82dbddf875e48d7be30f83ce774c0712005
zcrpro <zcrpro@gmail.com>
2018-06-14 15:01:52 +0800

    init project

---
+++ app/src/main/java/com/zhihuianxin/xyaxf/MainActivity.java
@@ -0,0 +1,47 @@
+package com.zhihuianxin.xyaxf;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+
+    @BindView(R.id.text)
+    TextView text;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        text.setText("包名:" + getAppProcessName(this) + "服务器地址：" + BuildConfig.API_HOST);
+    }
+
+    /**
+     * 获取当前应用程序的包名
+     *
+     * @param context 上下文对象
+     * @return 返回包名
+     */
+    public static String getAppProcessName(Context context) {
+        //当前应用pid
+        int pid = android.os.Process.myPid();
+        //任务管理类
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        //遍历所有应用
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo info : infos) {
+            if (info.pid == pid)//得到当前应用
+                return info.processName;//返回包名
+        }
+        return "";
+    }
+}
